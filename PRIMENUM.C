beed89e83b8b4a37692f000a4d841b040194ab29
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-11-06 20:20:25 +0530

    Add files via upload

---
+++ PRIMENUM.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,flag=0;
+clrscr();
+printf("Enter the Number");
+scanf("%d",&n);
+
+	if(n==0 || n==1)
+		flag =1;
+	for(i=2;i<=n/2;i++)
+		if (n%i == 0)
+		{
+			flag = 1;
+			break;
+		}
+		if (flag == 0)
+		printf(" prime number");
+		else
+		printf("not a prime nuber");
+
+getch();
+}
